5a73632a3eac36f957a25918fecf607009072e7a
Knight-O <knightokis@gmail.com>
2014-09-24 18:14:44 +0800

    Finished Simulation Test Subroutine

--- freeradius/scripts/subsc_cache.pl
+++ freeradius/scripts/subsc_cache.pl
@@ -4,15 +4,25 @@ use vars qw(%RAD_REQUEST %RAD_CHECK);
 use Redis;
 use Config::Std { def_sep => '=' };
 #use lib '/home/okis/perl5/lib/perl5';
-use Log::Lite;
+use Log::Lite qw(logpath log);
 
+# constant of FreeRADIUS module returned value
 use constant RLM_MODULE_REJECT	=>	0;
 use constant RLM_MODULE_OK	=>		2;
 use constant RLM_MODULE_NOOP	=>	7;
 use constant RLM_MODULE_UPDATED	=>	8;
 
-my $logpath = '';
+# define root path where we write our logs.
+my $logpath = '/var/log/radius';
+# define root path where we read our configurations.
+my $confpath = '/Users/okischuang/Documents/Dev/freeradius/conf';
+# for storing redis server settings.
+my %redisEnv;
+# global redis connection.
 my $redis_con;
+# calling sub to set up configuration environment variables.
+setUpConfig();
+# calling sub to set up redis connection.
 setUpRedisConn();
 
 sub accounting {
@@ -36,24 +46,83 @@ sub accounting {
 	}
 }
 
+sub log_err {
+	my $errMsg = $_[0];
+	logpath("$logpath");
+	log("error","subsc_cache","$errMsg");
+}
+
+sub setUpConfig {
+	eval {
+		# do something risky...
+		read_config "$confpath/redis.cfg" => %redisEnv;
+	};
+	if ($@) {
+		# handle failure...
+		log_err("$@");
+	}
+}
+
 sub setUpRedisConn {
+	my $s1_ip = $redisEnv{'Server1'}{'host'};
+	my $s1_port = $redisEnv{'Server1'}{'port'};
+	my $s1_reconn = $redisEnv{'Server1'}{'reconnect'};
 	eval {
 		# try to get a connection from Redis server.
 		$redis_con = Redis->new(
-			sever => 127.0.0.1,
-			reconnect => 3,	
+			sever => "$s1_ip:$s1_port",
+			reconnect => 3,
+			name => 'conn_subsc_cache',
 		);
 	};
 	if ($@) {
 		# handle failure...
+		log_err("$@");
+		# try to connect Server2
 	}
-	
+}
+
+sub checkInput {
+	if($ARGV[0] eq '-r'){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+sub radiusSimulation {
+	# Simulate hash value in FreeRADIUS.
+	$RAD_REQUEST{'NAS-IP-Address'} = checkInput()==1 ? $ARGV[1] : $ARGV[0];
+	$RAD_REQUEST{'Calling-Station-Id'} = checkInput()==1 ? genRandomMAC() : $ARGV[1];
+	$RAD_REQUEST{'Acct-Status-Type'} = $ARGV[2];
+
+	print "===Simulation START\n";
+	printf "NAS-IP-Address: %s | Calling-Station-Id: %s | Acct-Status-Type: %s\n", 
+		$RAD_REQUEST{'NAS-IP-Address'},$RAD_REQUEST{'Calling-Station-Id'}, $RAD_REQUEST{'Acct-Status-Type'};
+	my $ret = accounting();
+	if ($ret == 2) {
+		# body...
+		if ($RAD_REQUEST{'Acct-Status-Type'} eq 'Start') {
+			# body...
+			print "subscriber-$RAD_REQUEST{'Calling-Station-Id'} has been added into set $RAD_REQUEST{'NAS-IP-Address'}\n";
+		} else {
+			# else...
+			print "subscriber-$RAD_REQUEST{'Calling-Station-Id'} has been removed from set $RAD_REQUEST{'NAS-IP-Address'}\n";
+		}
+	} else {
+		# else...
+		print "Check if anything goes wrong!\n";
+	}
+	print "===Simulation END\n";
 }
 
 sub testFlow {
 	my $key = $_[0];
+
 	addRandElem2Set($key,5);
 	my @members = getAllMembersFromSet($key);
+	
 	print "SET $key contains ".($#members+1)." members: \n";
 	for(@members){
 		print "$_\n";
@@ -101,9 +170,10 @@ sub genRandomMAC {
 			my $a = $chars[rand(@chars)];
 			my $b = $chars[rand(@chars)];
 			$seg = "$a$b";
+			$seg = uc($seg);
 		}
 		$mac .= $seg if $i==1;
-		$mac .= "$sep$seg" if $i>1;
+		$mac .= "$sep-$seg" if $i>1;
 	}
 	return $mac;
 }
@@ -143,6 +213,13 @@ sub isMemberExist {
 	my $member = $_[1];
 	return 1 if $redis_con->sismember($key,$member) == 1;
 }
+
+###### For Test #######
+my %RAD_REQUEST;
+my %RAD_CHECK;
+die "INPUT: -r[random MAC]|KEY|MAC[must give without -r]|Acct-Status-Type\n" if scalar @ARGV < 3;
+radiusSimulation();
+#######################
 
 =setUpRedisConn
 =END
